d0108c3dae1e316977cbd7e9c2e5de23ab45edaa
mengchenwang <mengchenwang@hotmail.com>
2017-08-23 21:20:26 +0100

    add loading file

--- directory.rb
+++ directory.rb
@@ -187,6 +187,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -198,6 +199,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -216,6 +219,15 @@ def interactive_menu
     print_menu
     process(gets.chomp)
   end
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 def save_students
